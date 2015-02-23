a640b3e0523d03b7311a9c0638cd255596b0eb54
Tawni Martin <tawni@ideate-interactive.com>
2015-02-23 16:01:09 -0500

    question 2 and 3

--- js/foo.js
+++ js/foo.js
@@ -7,8 +7,23 @@ window.Foo = (function() {
   Foo.prototype = {
 
   	plusOne: function(num) {
-  		return num ++;
+  		return num + 1;
+  	},
+
+  	sortArray: function(unsorted) {
+
+  		 var clonedArray = _.clone(unsorted);
+  		 return clonedArray.sort();
+
+  	},
+
+  	hasValue: function(array, materialToCheck) {
+     var found = _.contains(array, materialToCheck);
+	   return found;
+  	
   	}
+
+
 
   };
 
