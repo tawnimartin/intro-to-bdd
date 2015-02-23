4e1dd625ee712ca151854b2ef5e71819dca9eedd
Tawni Martin <tawni@ideate-interactive.com>
2015-02-23 17:53:10 -0500

    Completed tests on cubed and reverseString

--- test/test.bar.js
+++ test/test.bar.js
@@ -14,7 +14,40 @@ describe("Bar", function() {
 
   });//
 
+  describe("cubed", function() {
 
+    it("returns the value of the number cubed", function() {
+      expect( bar.cubed(2) ).eq(8);
+      expect()
+    });
+
+  });//
+
+
+  // describe("exponentOf", function() {
+
+  //   it("returns the value of the number to the exponent of another number", function() {
+  //     function testFunction() {
+  //       return 2;
+  //     }
+  //     expect( bar.exponentOf(testFunction,3) ).eq(8);
+  //     expect()
+  //   });
+
+  // });//going to come back to this one
+
+
+    describe("reverseString", function() {
+
+    it("retuns the string reversed", function() {
+      var originalString = "string";
+      var reversedString = "gnirts"
+      
+      expect( bar.reverseString(originalString) ).eq(reversedString);
+      expect()
+    });
+
+  });//
 
 
 
