773ff09a069effe40e2b4091a421da43b928284f
Tawni Martin <tawni@ideate-interactive.com>
2015-02-23 21:09:14 -0500

    All test and functions complete

--- test/test.bar.js
+++ test/test.bar.js
@@ -12,7 +12,7 @@ describe("Bar", function() {
       expect()
     });
 
-  });//
+  });
 
   describe("cubed", function() {
 
@@ -21,23 +21,28 @@ describe("Bar", function() {
       expect()
     });
 
-  });//
+  });
 
 
-  // describe("exponentOf", function() {
+  describe("exponentOf", function() {
+    it("returns the value of a variable number to the exponent of another variable number", function() {
+      //
+      var powerOf3 = bar.exponentOf(3);
+      //for example, running bar.exponentOf(3) returns the "inner" function.. 
+      //that would look like this:
+      //  function(num) {
+      //  return Math.pow(num, 3);<--power of 3
+      //  };
+      //then the test would be to test the new function you created
+      //passing it the number you want to get to the power of 3
+      expect( powerOf3(2) ).eq(8);
+      expect()
+    });
 
-  //   it("returns the value of the number to the exponent of another number", function() {
-  //     function testFunction() {
-  //       return 2;
-  //     }
-  //     expect( bar.exponentOf(testFunction,3) ).eq(8);
-  //     expect()
-  //   });
-
-  // });//going to come back to this one
+  });
 
 
-    describe("reverseString", function() {
+  describe("reverseString", function() {
 
     it("retuns the string reversed", function() {
       var originalString = "string";
@@ -47,9 +52,44 @@ describe("Bar", function() {
       expect()
     });
 
-  });//
+  });
 
 
+  describe("cipher", function() {
+
+    it("retuns a ciphered string - with each letter converted to unicode+100", function() {
+      var originalString = "string";
+      var cipheredString = "×ØÖÍÒË"
+      
+      expect( bar.cipher(originalString) ).eq(cipheredString);
+      expect()
+    });
+
+  });
+
+  describe("decipher", function() {
+
+    it("returns a deciphered string - ciphered using the function above", function() {
+      var originalString = "×ØÖÍÒË";
+      var cipheredString = "string"
+      
+      expect( bar.decipher(originalString) ).eq(cipheredString);
+      expect()
+    });
+
+  });
+
+  describe("rot13", function() {
+
+    it("returns a rot13 ciphered string", function() {
+      var originalString = "tawni";
+      var cipheredString = "gnjav"
+      
+      expect( bar.rot13(originalString) ).eq(cipheredString);
+      expect()
+    });
+
+  });
 
 
 });
